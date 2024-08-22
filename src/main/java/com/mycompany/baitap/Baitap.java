7889c353ea9e5c88e23f27fdc30ad168e3532f00
KhoaTran14 <tranhuubachkhoa2005@gmail.com>
2024-08-23 01:26:45 +0700

    Assignments 2

---
+++ src/main/java/com/mycompany/baitap/Baitap.java
@@ -0,0 +1,12 @@
+package com.mycompany.baitap;
+public class Baitap {
+
+    public static void main(String[] args) {
+        int a = 122, b = 4;
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+    }
+}
